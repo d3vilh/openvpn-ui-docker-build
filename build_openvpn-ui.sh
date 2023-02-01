568523fa4cef84ba5b50f30f06591b128dfd6564
d3vilh <d3vilh@gmail.com>
2023-02-01 09:17:17 +0200

    build optimisation

--- build_openvpn-ui.sh
+++ build_openvpn-ui.sh
@@ -2,19 +2,22 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Set the home directory of the openvpn-ui project
+OVPN_UI_HOME="/home/admin/build/openvpn-ui"
+
+# Set the current directory
 OD=$PWD
 
 # go to script folder, required for below steps
 CURDIR=${OD}/$(dirname $0)
 cd "${CURDIR}"
+
 # Build golang & bee environment
 docker build --platform="arm" -f Dockerfile-beego -t local/beego-v8 -t local/beego-v8:latest .
-
 ./openvpn-ui-pack2.sh
 
 # Build OpenVPN-UI image
-
-PKGFILE="/home/philipp/build/openvpn-ui/openvpn-ui.tar.gz"
+PKGFILE=$OVPN_UI_HOME"openvpn-ui.tar.gz"
 cp -f $PKGFILE ./
 
 docker build -t local/openvpn-ui .
